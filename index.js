082ef6e2974c3eab1c531d92f64d7afe489a151a
Bitnami <howardc93@gmail.com>
2015-05-03 23:40:39 -0400

    laid out basic structure

--- index.js
+++ index.js
@@ -1,69 +1,50 @@
-var http = require('http');
 var path = require('path');
 var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
-
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
-
-router.use(express.static(path.resolve(__dirname, 'public')));
+var app = express();
+var server = app.listen(process.env.PORT || 5000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('[WEB] listening at http://%s:%s', host, port);
+});
+var io = require('socket.io')(server);
+app.use(express.static(path.resolve(__dirname, 'public')));
 var messages = [];
 var sockets = [];
-
+//io.emit broadcasts to all clients
 io.on('connection', function(socket) {
+  //within this, emits only to the connecting socket
   messages.forEach(function(data) {
     socket.emit('message', data);
   });
-
   sockets.push(socket);
-
   socket.on('disconnect', function() {
     sockets.splice(sockets.indexOf(socket), 1);
     updateRoster();
   });
-
   socket.on('message', function(msg) {
+    //message passed from a player
     var text = String(msg || '');
-
-    if (!text)
-      return;
-
+    if (!text) return;
     var data = {
       name: socket.name,
       text: text
     };
-
-    broadcast('message', data);
+    io.emit('message', data);
     messages.push(data);
   });
-
   socket.on('identify', function(name) {
+    //new player joined game!
     socket.name = String(name || 'Anonymous');
     updateRoster();
   });
 });
 
 function updateRoster() {
-  async.map(
-    sockets,
-    function(socket, callback) {
-      callback(null, socket.name);
-    },
-    function(err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function(socket) {
-    socket.emit(event, data);
+  //iterate through each connected client and get their name, then broadcast the roster to everyone
+  async.map(sockets, function(socket, callback) {
+    callback(null, socket.name);
+  }, function(err, names) {
+    io.emit('roster', names);
   });
 }
-
-server.listen(process.env.PORT || 5000, function() {
-  var addr = server.address();
-  console.log("Server listening at", addr.address + ":" + addr.port);
-});
