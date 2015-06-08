126774eebedd9f26917fd31b6f3a720682ec00a4
Bitnami <howardc93@gmail.com>
2015-06-07 21:34:53 -0400

    dealing hands

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var black = cards.filter(function(c) {
 var white = cards.filter(function(c) {
   return c.cardType === "A";
 });
-var messages = [];
+//TODO implement multiple rooms
 var players = [];
 //published state
 var board = {
@@ -29,71 +29,89 @@ var board = {
 };
 //GAME START
 //shuffle decks
-function shuffle(o) {
-  for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-  return o;
-}
 shuffle(black);
 shuffle(white);
-
-function replenish() {
-    //server deals hand to all players from white
-    players.forEach(function(p) {
-      var hand_max = 10;
-      while (p.hand.length < hand_max) {
-        p.hand.push(white.pop());
-      }
-    });
-  }
-  //EACH TURN
-  //server draws top black card
+//
+//EACH TURN
+//restore cards to hands
+replenish();
+//server draws top black card
 board.black = black.pop();
+//czar becomes next player
+//increment turn number
+//count number of cards remaining in play
+//notify players of board state
+io.emit('board', board);
+//notify players of player list/board state
+updateRoster();
 //each turn, keep track of whether each player has moved
-//each player has a hand, score
-//game state
-//server manages deck, hand state of all players
-//server will inform players of their hand state+board state
-//server replenishes player hands
+//ALL PLAYERS MOVED
+//emit event to all players
+//reveal all played cards
+//czar selects winner
+//loop next turn
+//
 //io.emit broadcasts to all clients
 //socket.emit messages only the particular socket
 io.on('connection', function(socket) {
   //new player joined
-  console.log(socket.id);
+  console.log("%s joined", socket.id);
   socket.score = 0;
   socket.hand = [];
   players.push(socket);
-  updatePlayers();
+  replenish();
+  updateRoster();
   //set up handler for player disconnect
   socket.on('disconnect', function() {
     players.splice(players.indexOf(socket), 1);
-    updatePlayers();
+    console.log("%s left", socket.id);
+    updateRoster();
   });
-  socket.on('message', function(msg) {
+  socket.on('play', function(msg) {
     //message/event passed from a player
-    var text = String(msg || '');
-    if (!text) return;
-    var data = {
-      name: socket.name,
-      text: text
-    };
-    io.emit('message', data);
-    messages.push(data);
+    //index of card in hand played
+    //notify all players that this player moved
+    //TODO implement
+  });
+  socket.on('select', function(msg) {
+    //ensure the select comes from the current czar
+    //czar selects a winning card
   });
   socket.on('name', function(name) {
     //player changing name
     socket.name = String(name);
-    updatePlayers();
+    updateRoster();
   });
 });
 
-function updatePlayers() {
+function updateRoster() {
   //iterate through each connected client and get their name, then broadcast the roster to everyone
-  async.map(players, function(socket, callback) {
-    callback(null, socket.name);
+  async.map(players, function(p, cb) {
+    //notify each player of their hand
+    console.log(p.hand);
+    p.emit('hand', p.hand);
+    cb(null, p.name);
   }, function(err, names) {
     if (err) {
       console.log(err);
     }
+    //notify everyone of current players list
     io.emit('roster', names);
   });
+}
+
+function replenish() {
+  //server deals hand to all players from white
+  players.forEach(function(p) {
+    var hand_max = 10;
+    while (p.hand.length < hand_max) {
+      p.hand.push(white.pop());
+    }
+    console.log(p.hand);
+  });
+}
+
+function shuffle(o) {
+  for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  return o;
 }
