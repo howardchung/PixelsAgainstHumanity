77706749b9b266505cb4cbc39cb308a99277b6a1
Bitnami <howardc93@gmail.com>
2015-06-08 05:18:34 -0400

    swapped card sets

--- index.js
+++ index.js
@@ -11,48 +11,28 @@ var server = app.listen(process.env.PORT || 5000, function() {
 var io = require('socket.io')(server);
 app.use(express.static(path.resolve(__dirname, 'public')));
 //private server state
-var black = cards.filter(function(c) {
-  return c.cardType === "Q";
+var deck = "Base";
+var black = cards[deck].black.map(function(c) {
+  return cards.blackCards[c];
 });
-var white = cards.filter(function(c) {
-  return c.cardType === "A";
+var white = cards[deck].white.map(function(c) {
+  return cards.whiteCards[c];
 });
 //TODO implement multiple rooms
 var players = [];
 //published state
 var board = {
   black: null,
-  czar: null,
+  whites: [],
+  czar: -1,
   turn: 0,
   black_remaining: null,
   white_remaining: null
 };
-//GAME START
-//shuffle decks
 shuffle(black);
 shuffle(white);
-//
-//EACH TURN
-//restore cards to hands
-replenish();
-//server draws top black card
-board.black = black.pop();
-//czar becomes next player
-//increment turn number
-//count number of cards remaining in play
-//notify players of board state
-io.emit('board', board);
-//notify players of player list/board state
-updateRoster();
-//each turn, keep track of whether each player has moved
-//ALL PLAYERS MOVED
-//emit event to all players
-//reveal all played cards
-//czar selects winner
-//loop next turn
-//
-//io.emit broadcasts to all clients
-//socket.emit messages only the particular socket
+//TODO start game when a player/leader wants to
+runTurn();
 io.on('connection', function(socket) {
   //new player joined
   console.log("%s joined", socket.id);
@@ -68,14 +48,21 @@ io.on('connection', function(socket) {
     updateRoster();
   });
   socket.on('play', function(msg) {
-    //message/event passed from a player
     //index of card in hand played
     //notify all players that this player moved
+    //each turn, keep track of how many players still need to move
+    //WHEN ALL PLAYERS MOVED
+    //emit event to all players
+    //reveal all played cards
+    //czar selects winner
     //TODO implement
   });
   socket.on('select', function(msg) {
     //ensure the select comes from the current czar
     //czar selects a winning card
+    //reveal winner, increment score
+    //start the next turn
+    //TODO 
   });
   socket.on('name', function(name) {
     //player changing name
@@ -83,6 +70,24 @@ io.on('connection', function(socket) {
     updateRoster();
   });
 });
+
+function runTurn() {
+  //restore cards to hands
+  replenish();
+  //server draws top black card
+  board.black = black.pop();
+  //czar becomes next player
+  board.czar = (board.czar + 1) % players.length;
+  //increment turn number
+  board.turn += 1;
+  //count number of cards remaining in play
+  board.black_remaining = black.length;
+  board.white_remaining = white.length;
+  //notify players of board state
+  io.emit('board', board);
+  //notify players of player list/hand/board state
+  updateRoster();
+}
 
 function updateRoster() {
   //iterate through each connected client and get their name, then broadcast the roster to everyone
