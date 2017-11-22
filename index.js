4876559b0765c138a302486da34e64ca97b0d839
Howard Chung <howardc93@gmail.com>
2017-11-22 07:05:00 +0000

    use ws/websocket directly

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ var async = require('async');
 var express = require('express');
 var cards = require('./cards.json');
 var app = express();
-var server = app.listen(process.env.PORT || 5000, function() {
+const WebSocket = require('ws');
+const http = require('http');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+server.listen(process.env.PORT || 6000, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('[WEB] listening at http://%s:%s', host, port);
 });
-var io = require('socket.io')(server);
-app.use(express.static(path.resolve(__dirname, 'public')));
 //user joins, gets list of rooms
 //user can change name at any time
 //user joins a room by default as spectator
@@ -17,82 +30,86 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 //inside room, any player can start game
 //new game automatically called on room creation
 //TODO implement multiple rooms
+//TODO allow spectating
 var black;
 var white;
 var players = [];
 var tempWhites;
 var board;
 newGame();
-io.on('connection', function(socket) {
+wss.on('connection', function(socket) {
   //new player joined
   console.log("%s joined", socket.id);
-  //TODO players join as spectators by default, enter player list onclick
+  socket.on('message', function incoming(data) {
+    const message = JSON.parse(data);
+    if (message.type === 'disconnect') {
+      players.splice(players.indexOf(socket), 1);
+      console.log("%s left", socket.id);
+      //TODO handle czar leaving game
+      //TODO handle player leaving game
+      updateRoster();
+    }
+    else if (message.type === 'start') {
+      //only allow czar to advance turn, unless starting new game?
+      //or only allow creator to start new game
+      if (socket.status === "czar" || board.czar === -1 || true) {
+        runTurn();
+        updateRoster();
+      }
+    }
+    else if (message.type === 'play') {
+      let index = message.index;
+      var whites = tempWhites;
+      var playerIndex = players.indexOf(socket);
+      //ensure the player has cards left to play and is not czar
+      if ((!whites[playerIndex] || whites[playerIndex].length < board.black.pick) && socket.status !== "czar") {
+        index = Number(index);
+        //TODO use scrambled indices
+        if (!whites[playerIndex]) {
+          whites[playerIndex] = [];
+        }
+        whites[playerIndex].push(socket.hand[index]);
+        //remove card from player's hand
+        socket.hand.splice(index, 1);
+        //if player has no cards left to play
+        if (whites[playerIndex].length >= board.black.pick) {
+          //notify all players that this player moved
+          socket.status = "ready";
+          //if all active players have moved
+          if (checkReady()) {
+            //reveal all played cards
+            board.whites = tempWhites;
+          }
+        }
+        updateRoster();
+      }
+    }
+    else if (message.type === 'select') {
+      let index = message.index;
+      index = Number(index);
+      //TODO use scrambled indices
+      //check that there hasn't been a winner selected this turn
+      //make sure this player is czar
+      if (socket.status === "czar" && !board.selected) {
+        players[index].score += 1;
+        players[index].winner = true;
+        //reveal winner, increment score
+        //remove all from board except winner
+        board.whites = [board.whites[index]];
+        board.selected = true;
+        updateRoster();
+      }
+    }
+    else if (message.type === 'name') {
+      //player changing name
+      socket.name = String(message.name);
+      updateRoster();
+    }
+  });
   socket.score = 0;
   socket.hand = [];
   players.push(socket);
   updateRoster();
-  //set up handler for player disconnect
-  socket.on('disconnect', function() {
-    players.splice(players.indexOf(socket), 1);
-    console.log("%s left", socket.id);
-    //TODO handle czar leaving game
-    //TODO handle player leaving game
-    updateRoster();
-  });
-  socket.on('start', function() {
-    //only allow czar to advance turn, unless starting new game?
-    //or only allow creator to start new game
-    if (socket.status === "czar" || board.czar === -1 || true) {
-      runTurn();
-      updateRoster();
-    }
-  });
-  socket.on('play', function(index) {
-    var whites = tempWhites;
-    var playerIndex = players.indexOf(socket);
-    //ensure the player has cards left to play and is not czar
-    if ((!whites[playerIndex] || whites[playerIndex].length < board.black.pick) && socket.status !== "czar") {
-      index = Number(index);
-      //TODO use scrambled indices
-      if (!whites[playerIndex]) {
-        whites[playerIndex] = [];
-      }
-      whites[playerIndex].push(socket.hand[index]);
-      //remove card from player's hand
-      socket.hand.splice(index, 1);
-      //if player has no cards left to play
-      if (whites[playerIndex].length >= board.black.pick) {
-        //notify all players that this player moved
-        socket.status = "ready";
-        //if all active players have moved
-        if (checkReady()) {
-          //reveal all played cards
-          board.whites = tempWhites;
-        }
-      }
-      updateRoster();
-    }
-  });
-  socket.on('select', function(index) {
-    index = Number(index);
-    //TODO use scrambled indices
-    //check that there hasn't been a winner selected this turn
-    //make sure this player is czar
-    if (socket.status === "czar" && !board.selected) {
-      players[index].score += 1;
-      players[index].winner = true;
-      //reveal winner, increment score
-      //remove all from board except winner
-      board.whites = [board.whites[index]];
-      board.selected = true;
-      updateRoster();
-    }
-  });
-  socket.on('name', function(name) {
-    //player changing name
-    socket.name = String(name);
-    updateRoster();
-  });
 });
 
 function newGame() {
@@ -154,7 +171,7 @@ function updateRoster() {
   async.map(players, function(p, cb) {
     //notify each player of their hand
     console.log(p.hand);
-    p.emit('hand', p.hand);
+    p.write('hand', JSON.stringify(p.hand));
     cb(null, {
       name: p.name,
       score: p.score,
@@ -166,8 +183,8 @@ function updateRoster() {
       console.log(err);
     }
     //notify every player of roster/board state
-    io.emit('roster', names);
-    io.emit('board', board);
+    wss.broadcast('roster', names);
+    wss.broadcast('board', board);
   });
 }
 
